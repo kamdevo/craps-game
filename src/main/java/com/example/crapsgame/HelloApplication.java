92c8ccae8c0dcf7efe9ddfcb85cb03fceb71cf3e
kamdevo <camilomoralesyk@gmail.com>
2026-02-28 12:59:32 -0500

    commit4545

--- src/main/java/com/example/crapsgame/HelloApplication.java
+++ src/main/java/com/example/crapsgame/HelloApplication.java
@@ -3,15 +3,24 @@ package com.example.crapsgame;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
-
+import javafx.scene.layout.VBox;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        VBox root = new VBox();
+        Label infoLabel = new Label("click here");
+        Button helloButton = new Button("Greet");
+
+        root.getChildren().add(helloButton);
+        root.getChildren().add(infoLabel);
+
+        Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
